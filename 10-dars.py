80f5111d8a7316f59192b89f4273404f6c756df3
Olimjon <olimtoy10@mail.ru>
2022-06-16 18:21:47 +0500

    add 10-dars

---
+++ 10-dars.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#son = int(input("Istalgan s k:"))
+#if son>=0:
+#    print("Musbat son")
+#else:
+#    print("Manfiy son")
+
+#son = float(input("Istalgan s k:"))
+#if son>=0:
+#    print("Musbat son")
+#else:
+#    print("Manfiy son")
+
+#son = 6666*125522
+#print(son)
+
+#mevalar = ['olma', 'anor', 'nok']
+#print(mevalar[1])\
+
+#son = float(input("Istalgan son kiriting:"))
+#ildiz = son**0.5 #muhim ma'lumot
+#print(f"{son} ning ildizi {ildiz} ga teng")
