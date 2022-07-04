4ef69de77f9a158230bc26ac74b83ea1ef3972ec
Olimjon <olimtoy10@mail.ru>
2022-07-04 16:55:12 +0500

    Create 15-dars_Lug'atlar_bilan_ishlash.py
    Lug'atlar bilan ishlash

---
+++ 15-dars_Lug'atlar_bilan_ishlash.py
@@ -0,0 +1,23 @@
+talaba_0 = {
+   'ism':'alijon',
+   'familiya':'shamsiyev',
+   'yosh':22,
+   'fakultet':'matematika',
+   'kurs':4
+   }
+
+#print(talaba_0.items())
+
+#for kalit, qiymat in talaba_0.items():
+#   print(f"Kalit: {kalit}")
+#   print(f"Qiymat: {qiymat} \n")
+
+telefonlar = {
+    'ali':'iphone x',
+    'vali':'galaxy s9',
+    'olimjon':'Iphone 13pro',
+    'orif':'nokia 3310'
+    }
+
+for k, q in telefonlar.items():
+    print(f"{k.title()}ning telefoni {q}")
